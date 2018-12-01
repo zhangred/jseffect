20e561ddf8c53b43b05baac94b9d261b35a9d155
zhangred <577056210@qq.com>
2018-12-01 14:08:39 +0800

    'rili'

---
+++ testrili/assets/javascript/func.js
@@ -0,0 +1,107 @@
+﻿var CUES = {
+    tip:function(options){
+        var msg = options.msg;
+        var type = options.type?options.type:'tip';
+        var time = options.time?options.time:1000;
+        var elma = this.createdom({"tag":"div","classname":"tippop","msg":'<div class="text '+type+'">'+msg+'</div></div>'});
+        document.body.appendChild(elma);
+        setTimeout(function(){
+            elma.style.opacity = 0;
+            elma.style.webkitTransition = 'all 1.5s';
+            setTimeout(function(){
+                options.callback && options.callback();
+                elma.remove();
+            },1000);
+        },time);
+    },
+    alert:function(options){
+        var msg = options.msg;
+        var elma = this.createdom({"tag":"div","classname":"tippop_alert"});
+        var elmb = this.createdom({"tag":"p","classname":"tippop_alert_t","msg":options.msg});
+        var elmc = this.createdom({"tag":"a","classname":"tippop_alert_b","msg":"确定"});
+        elma.appendChild(elmb),elma.appendChild(elmc);
+        document.body.appendChild(elma);
+
+        var elm_bg = this.createdom({"tag":"div","classname":"tippop_bg"});
+        document.body.appendChild(elm_bg);
+
+        elmc.addEventListener('click',function(){
+            elm_bg.remove();
+            elma.remove();
+            options.callback && options.callback();
+        },false);
+    },
+    confirm:function(options){
+        var msg = options.msg;
+        var elma = this.createdom({"tag":"div","classname":"tippop_alert"});
+        var elmb = this.createdom({"tag":"p","classname":"tippop_alert_t","msg":options.msg});
+        var elmc = this.createdom({"tag":"p","classname":"tippop_alert_b"});
+        var a_cal = this.createdom({"tag":"a","classname":"tippop_confirm_btn","msg":"取消"});
+        var a_sure = this.createdom({"tag":"a","classname":"tippop_confirm_btn","msg":"确定"});
+        elmc.appendChild(a_cal),elmc.appendChild(a_sure);
+        elma.appendChild(elmb),elma.appendChild(elmc);
+        document.body.appendChild(elma);
+        var elm_bg = this.createdom({"tag":"div","classname":"tippop_bg"});
+        document.body.appendChild(elm_bg);
+        a_cal.addEventListener('click',function(){
+            elm_bg.remove();
+            elma.remove();
+            options.callback && options.callback(0);
+        },false);
+        a_sure.addEventListener('click',function(){
+            elm_bg.remove();
+            elma.remove();
+            options.callback && options.callback(1);
+        },false);
+    },
+    input:function(options){
+        var msg = options.msg;
+        var elma = this.createdom({"tag":"div","classname":"tippop_alert"});
+        var elmb = this.createdom({"tag":"p","classname":"tippop_input_t","msg":options.msg});
+        var input = this.createdom({"tag":"input","classname":"input"});
+        var elmm = this.createdom({"tag":"p","classname":"tippop_input_m"});
+        var elmc = this.createdom({"tag":"p","classname":"tippop_alert_b"});
+        var a_cal = this.createdom({"tag":"a","classname":"tippop_confirm_btn","msg":"取消"});
+        var a_sure = this.createdom({"tag":"a","classname":"tippop_confirm_btn","msg":options.suretext?options.suretext:"确定"});
+        elmc.appendChild(a_cal),elmc.appendChild(a_sure);
+        input.setAttribute('placeholder',options.placeholder?options.placeholder:'');
+        elmm.appendChild(input);
+        elma.appendChild(elmb),elma.appendChild(elmm),elma.appendChild(elmc);
+        document.body.appendChild(elma);
+        var elm_bg = this.createdom({"tag":"div","classname":"tippop_bg"});
+        document.body.appendChild(elm_bg);
+        a_cal.addEventListener('click',function(){
+            elm_bg.remove();
+            elma.remove();
+            options.callback && options.callback(0);
+        },false);
+        a_sure.addEventListener('click',function(){
+            elm_bg.remove();
+            elma.remove();
+            options.callback && options.callback(input.value);
+        },false);
+    },
+    createdom:function(options){
+        var dom = document.createElement(options.tag);
+        dom.className = options.classname;
+        if(options.msg){
+            dom.innerHTML = options.msg;
+        }
+        return dom;
+    }
+};
+function getRequest(){
+    var url = window.location.search,
+        theRequest = {},
+        str = '',
+        para = [];
+    if (url.indexOf("?") != -1) {
+        str = url.substr(1);
+        strs = str.split("&");
+        for(var i = 0, len = strs.length; i < len; i ++) {
+            para = strs[i].split("=");
+            theRequest[para[0]] = decodeURIComponent( (para.length>=2)?para[1]:"");
+        }
+    }
+    return theRequest;
+};
