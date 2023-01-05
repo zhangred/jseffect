361f874de35415fff5a1377aad5697422476a32c
张少华 <zhangshaohua@ikcrm.com>
2023-01-05 19:10:43 +0800

    tongji

---
+++ album/js/base.js
@@ -0,0 +1,62 @@
+﻿window.vues.base = new Vue({
+  el: '#vue_base',
+  data: {
+    pageName: 'base',
+    pageShow: false,
+    hasRender: false,
+    redChart: []
+  },
+  mounted: function(){
+  },
+  methods: {
+    initPageShow: function(name){
+      this.pageShow = name === this.pageName;
+    },
+    // 初次渲染
+    render: function(){
+      if (!this.hasRender) {
+        this.hasRender = true;
+        this.initData();
+      }
+    },
+    initData: function(){
+      let redCount = {};
+		  let blueCount = {};
+      for (let i = 0; i < allData.length; i++) {
+        let reds = allData[i].reds;
+        reds.forEach((item) => {
+          if (!redCount[item]) {
+            redCount[item] = 0;
+          }
+          redCount[item]++;
+        })
+        if (!blueCount[allData[i].blue]) {
+          blueCount[allData[i].blue] = 0;
+        }
+        blueCount[allData[i].blue]++;
+      }
+      let max = this.getMax(redCount) * 1.2;
+      this.redChart = this.getList(redCount, 33, max)
+    },
+    getMax: function(obj){
+      let max = 1;
+      Object.keys(obj).forEach(k => {
+        if (obj[k] > max) {
+          max = obj[k]
+        }
+      })
+      return max;
+    },
+    getList: function(obj, max, mwidth){
+      let list = [];
+      for (let i = 1; i <= max; i++) {
+        list.push({
+          name: i,
+          count: obj[i],
+          width: obj[i]/mwidth * 100
+        })
+      }
+      return list;
+    }
+  }
+});
---
+++ album/js/main的副本3.js
@@ -0,0 +1,26 @@
+﻿new Vue({
+  el: '#app',
+  data: {
+    tabList: [
+      {id: 1, label: '基础数据统计', name: 'base'},
+      {id: 2, label: '基础数据统计2', name: 'base2'},
+      {id: 3, label: '基础数据统计3', name: 'base3'},
+      {id: 4, label: '基础数据统计4', name: 'base4'},
+      {id: 5, label: '基础数据统计5', name: 'base5'},
+      {id: 6, label: '基础数据统计6', name: 'base6'},
+      {id: 7, label: '基础数据统计7', name: 'base7'},
+      {id: 8, label: '基础数据统计8', name: 'base8'},
+    ],
+    currentTabName: 'base'
+  },
+  mounted: function(){
+    window.vues[this.currentTabName]?.render?.();
+  },
+  methods: {
+    // 初次渲染
+    render: function(){},
+    changeTab: function(item){
+      this.currentTabName = item.name;
+    }
+  }
+});
